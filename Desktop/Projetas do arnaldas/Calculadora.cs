0beda898ec9ccc9469d5f38d0ee31ce71f491a47
Murilo Lima <murilodelimasousa@gmail.com>
2024-02-06 23:51:19 -0300

    Inicio do projeto

---
+++ Desktop/Projetas do arnaldas/Calculadora.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+namespace Arnaldo
+{
+    public class Calculadora
+    {
+        public delegate void Op(double num1, double num2, ref double R);
+
+        public double num1;
+        public double num2;
+        public Calculadora(double aNum1, double aNum2)
+        {
+            this.num1 = aNum1;
+            this.num2 = aNum2;
+            
+        }
+
+
+        public void Soma(double number1, double number2, ref double R)
+        {
+            R = number1 + number2;
+        }
+        public void Subtração(double number1, double number2, ref double R)
+        {
+            R = number1 - number2;
+        }
+        public void Multiplicação(double number1, double number2, ref double R)
+        {
+            R = number1 * number2;
+        }
+        public void Divisão(double number1, double number2, ref double R)
+        {
+            R = number1 / number2;
+        }
+        
+    }
+}
---
+++ Desktop/Projetas do arnaldas/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using static Arnaldo.Calculadora;
+
+namespace Arnaldo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            bool Ocorrendodowload = false;
+            Ocorrendodowload = true;
+            await Yes(Ocorrendodowload);
+            Ocorrendodowload = false;
+            if (Ocorrendodowload == false)
+            {
+                Console.ReadKey();
+            }
+        }
+        static async Task Yes(bool Ocorrendodowload)
+        {
+            Console.WriteLine("Espere 5 segundos para receber seu dowload");
+            await Task.Delay(5000);
+            Console.WriteLine("Https//:www.youtube.com//");
+        }
+    }
+}
